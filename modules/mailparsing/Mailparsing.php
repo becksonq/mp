68c7a365baed04e4cb38c31e2ad94326bcad10ba
beckson <becksonq@gmail.com>
2017-08-17 20:00:18 +0300

    start modul

---
+++ modules/mailparsing/Mailparsing.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\modules\mailparsing;
+
+/**
+ * Created by PhpStorm.
+ * User: Администратор_
+ * Date: 17.08.2017
+ * Time: 19:57
+ */
+class Mailparsing
+{
+
+}
--- views/site/index.php
+++ views/site/index.php
@@ -5,6 +5,8 @@ ini_set( 'error_reporting', E_ALL );
 ini_set( 'display_errors', 1 );
 ini_set( 'display_startup_errors', 1 );
 
+set_time_limit(0);
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
@@ -62,26 +64,25 @@ else {
 		// imap_headerinfo
 		$msg_header = imap_header( $connection, $i );
 
-		/*print '<pre>';
+		print '<pre>';
 		print_r( $msg_header );
-		print '</pre>';*/
+		print '</pre>';
 
 		$mails_data[$i]["time"] = time( $msg_header->MailDate );
 		$mails_data[$i]["date"] = $msg_header->MailDate;
 
 		foreach ( $msg_header->to as $data ) {
 			// Кому
-			$mails_data[$i]["to"] = $data->mailbox . "@" . $data->host;
+			$mails_data[$i]["to"] = $data->mailbox . "@" . $data->host; //print $mails_data[$i]["to"] . "<br>";
 		}
 
 		foreach ( $msg_header->from as $data ) {
 			// От кого
-			$mails_data[$i]["from"] = $data->mailbox . "@" . $data->host;
+			$mails_data[$i]["from"] = $data->mailbox . "@" . $data->host; //print $mails_data[$i]["from"] . "<br>";
 		}
 
 		if ( property_exists( $msg_header, 'subject' ) ) {
-			$mails_data[$i]["title"] = get_imap_title( $msg_header->subject );
-			//print $mails_data[$i]["title"] . "<br>";
+			$mails_data[$i]["title"] = get_imap_title( $msg_header->subject ); //print $mails_data[$i]["title"] . "<br>";
 		}
 
 		// Тело письма
@@ -94,81 +95,56 @@ else {
 			$parts = $msg_structure->parts;
 
 			// Количество вложенных файлов
-			$count_parts = count( $parts );
+//			$count_parts = count( $parts );
+			$count_parts = 5;
 
-//			for ( $h = 0; $h < $count_parts; $h++ ) {
-//
-//				if ( in_array( $msg_structure->parts[$h]->subtype, $mail_filetypes ) ) {
-//
-//					$count_subparts = count( $msg_structure->parts[$h] );
-//
-//					//print $count_subparts . "<br>";
-//
-//					for ( $g = 0; $g < $count_subparts; $g++ ) {
-//
-//						print '<pre>';
-////						print $msg_structure->parts[$g]->encoding;
-//						print $msg_structure->parts[$g]->subtype . "<br>";
-//						print $msg_structure->parts[$g]->type . "<br>";
-//
-//						$mess = structure_encoding( $msg_structure->parts[$g]->encoding, imap_fetchbody( $connection, $i, $h, $g ) );
-//
-//						file_put_contents( "tmp/" . iconv( "utf-8", "cp1251", $msg_structure->parts[$g]->type . 'i' ), $mess );
-//
-//						print '</pre>';
-//					}
-//
-//					//
-//				}
-//			}
+			for ( $j = 1, $f = 2; $j<$count_parts; $j++, $f++ ) {
 
-			for ( $j = 1, $f = 2; $j < $count_parts; $j++, $f++ ) {
+				if ( in_array( $parts[ $j ]->subtype, $mail_filetypes ) ) {
 
-				if ( in_array( $parts[$j]->subtype, $mail_filetypes ) ) {
+					if ( property_exists( $parts[$j], 'encoding' ) ) {
+//
+						$file_name = md5( time() ) . ".html";
+						$file = structure_encoding( $parts[$j]->encoding, imap_fetchbody( $connection, $i, $f ) );
 
-					foreach ( $parts[$j]->parts as $p ){
-//						if ( in_array( $p->subtype, ['HTML'] ) ) {
-
-//							print '<pre>';
-//						print $p->parts[0]->encoding;
-//							print_r( $p );
-////						print( get_imap_title( $p->parameters[0]->value ) );
-//							print '</pre>';
-//						}
-
-//						$file = structure_encoding(	$p->encoding, imap_fetchbody( $connection, $i, $f ));
-
-//						file_put_contents( "tmp/" . iconv( "utf-8", "cp1251", $parts[$j]->bytes ),	$file );
-
-						foreach ( $p as $pt ) {
-							if( is_array( $pt ) ) {
-								if( property_exists( $pt[0], 'encoding') ){
-									print $pt[0]->parameters[0]->value;
-								}
-//								if ( in_array( $pt[0]->subtype, ['PLANE'] ) ) {
-//									print '<pre>';
-//									print_r( $pt[0] );
-//									print '</pre>';
-//								}
-							}
-						}
-
+						file_put_contents( "tmp/" . $file_name, $file );
 					}
+
+//					foreach ( $parts[ $j ]->parts as $p ) {
+						/*if ( property_exists( $p, 'encoding' ) ) {
+//						
+							$file_name = md5( time() ) . ".html";
+							$file = structure_encoding( $p->encoding, imap_fetchbody( $connection, $i, $f ) );
+
+							file_put_contents( "tmp/" . $file_name, $file );
+						}*/
+
+						/*foreach ( $p as $key => $pt ) {
+							if ( is_array( $pt ) ) {
+								if ( property_exists( $pt[ 0 ], 'encoding' ) ) {
+									
+									$file_name = md5( time() ) . ".html";
+//									print '<pre>';
+//									print $pt[ 0 ]->parameters[ 0 ]->attribute;
+//									print '</pre>';
+
+									$file = structure_encoding( $pt[ 0 ]->encoding, imap_fetchbody( $connection, $i, $f ) );
+
+									file_put_contents( "tmp/" . $file_name, $file );
+//									file_put_contents( "tmp/" . iconv( "utf-8", "cp1251", $pt[ 0 ]->bytes ), $file );
+								}
+//
+							}
+						}*/
+
+//					}
 
 
 					// RFC822
-					$mails_data[$i]["attachs"][$j]["type"] = $parts[$j]->subtype; //print $mails_data[$i]["attachs"][$j]["type"] . "<br>";
-					$mails_data[$i]["attachs"][$j]["size"] = $parts[$j]->bytes;
+//					$mails_data[$i]["attachs"][$j]["type"] = $parts[$j]->subtype; //print $mails_data[$i]["attachs"][$j]["type"] . "<br>";
+//					$mails_data[$i]["attachs"][$j]["size"] = $parts[$j]->bytes;
 
-					$parameters = $parts[$j]->parameters;
-
-//					print_r($parameters);
-//					if (property_exists($parameters, 'attribute')) {
-//
-//						print "a" . "<br>";
-//					}
-
-//					print gettype( $msg_structure->parts[$j]->parameters->value ); exit;
+//					$parameters = $parts[$j]->parameters;
 
 //					if (property_exists($msg_structure->parts[$j]->parameters[0], 'value')) {
 //
@@ -182,10 +158,6 @@ else {
 //							$msg_structure->parts[$j]->encoding, imap_fetchbody( $connection, $i, $f )
 //					);
 
-//					print '<pre>';
-//					print $mails_data[$i]["attachs"][$j]["file"];
-//					print '</pre>'; //exit;
-
 
 					// Сохраняем файл в папку tmp/
 //					file_put_contents( "tmp/" . iconv( "utf-8", "cp1251", $msg_structure->parts[$j]->bytes ),	$mails_data[$i]["attachs"][$j]["file"] );
@@ -193,14 +165,8 @@ else {
 				}
 			}
 		}
-
-
 	}
 }
-
-//print '<pre>';
-//print_r( $mails_data );
-//print '</pre>';
 
 imap_close( $connection );
 
